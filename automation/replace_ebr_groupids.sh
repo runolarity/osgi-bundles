23e49662f50eeeeaad2c3433f6372c73970e4248
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2014-06-05 15:19:50 +0200

    Add automation script from EBR for better testability
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

---
+++ automation/replace_ebr_groupids.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eux
+
+#
+# This script can be used to replace the Eclipse group id with soomething else.
+# 
+
+echoerr() { echo "$@" 1>&2; }
+
+if [ -z ${1+x} ] || [ -z "$1" ]; then
+  echoerr "ERROR: missing group id"
+  echo "$(basename "$0") <new.group.id>"
+else
+  MYPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+  pushd "$MYPATH"
+  find "../ebr/recipes" -name "pom.xml" -print0 | xargs -0 echo 
+  #sed -i '' -e "s/MY_EBR_BUNDLES_GROUP/$1/g"
+fi
+
